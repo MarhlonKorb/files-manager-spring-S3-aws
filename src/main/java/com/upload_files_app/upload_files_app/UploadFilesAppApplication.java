fd83b78f57f984ae32a9690e8cd806e10dc74863
Marhlon Korb <marhlonkorb@hotmail.com>
2024-09-01 08:31:20 -0300

    Initial commit

---
+++ src/main/java/com/upload_files_app/upload_files_app/UploadFilesAppApplication.java
@@ -0,0 +1,13 @@
+package com.upload_files_app.upload_files_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFilesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFilesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upload_files_app/upload_files_app/UploadFilesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload_files_app.upload_files_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFilesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
